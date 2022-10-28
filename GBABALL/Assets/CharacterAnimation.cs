9ea760a84f530488f5dfe6ff1da7dfa2e6a55bd3
BobRossTSN <116233056+BobRossTSN@users.noreply.github.com>
2022-10-27 19:01:22 -0700

    AnimationWorking

--- GBABALL/Assets/CharacterAnimation.cs
+++ GBABALL/Assets/CharacterAnimation.cs
@@ -14,21 +14,32 @@ public class CharacterAnimation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if(characteranimator != null)
         {
-            if(Input.GetKeyDown(KeyCode.W))
+            if(Input.GetKey(KeyCode.W))
             {
                 characteranimator.SetTrigger("TrForward");
             }
-
-            if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+            // else if(Input.GetKeyUp(KeyCode.W) && Input.GetKeyDown(KeyCode.A) && Input.GetKeyDown(KeyCode.S) && Input.GetKeyDown(KeyCode.D))
+            // {
+            //     characteranimator.SetTrigger("TrIdle");
+            // }
+            if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
             {
-
+                characteranimator.ResetTrigger("TrIdle");
             }
             else
            {
                 characteranimator.SetTrigger("TrIdle");
+                characteranimator.ResetTrigger("TrForward");
            }
+        //     if(characteranimator.GetBool("TrIdle") == false)
+        //     {
+        //         characteranimator.SetTrigger("TrIdle");
+        //     }
+        //    print(characteranimator.GetBool("TrIdle"));
+
         }
     }
 }
